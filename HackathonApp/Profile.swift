0d5629f689258427a168ab29a0e9e8bad31006e4
Leoul Yiheyis <leoul16@vt.edu>
2017-06-07 13:46:10 -0400

    added to Profile class

--- HackathonApp/Profile.swift
+++ HackathonApp/Profile.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017 klangholm. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct tab {
+    var person: String
+    var description: String
+    var price: Int
+}
+
+class Profile : NSObject {
+    
+    fileprivate var name: String
+    fileprivate var pic: UIImage?
+    fileprivate var runningTabs: Array<tab>?
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    
+    func addTab(new: tab) {
+        letlim = runningTabs?.count
+        
+        runningTabs?[lim!] = new
+        
+    }
+    
+    var getName: String {
+        return self.name
+    }
+    
+    var getRunningTabs: Array<tab> {
+        return self.runningTabs!
+    }
+    
+    
+}
